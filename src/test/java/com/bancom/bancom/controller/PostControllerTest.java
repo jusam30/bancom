edefe3d9e0fce719401ca5e2c246db9734e5cc17
Juan <jusam30@gmail.com>
2024-02-08 17:54:01 -0500

    Versión 1

---
+++ src/test/java/com/bancom/bancom/controller/PostControllerTest.java
@@ -0,0 +1,28 @@
+package com.bancom.bancom.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @Test
+    void guardarPost() {
+    }
+
+    @Test
+    void consultarPost() {
+    }
+
+    @Test
+    void modificarPost() {
+    }
+
+    @Test
+    void eliminarPost() {
+    }
+
+    @Test
+    void listarUsuario() {
+    }
+}
--- src/test/java/com/bancom/bancom/controller/UsuarioControllerTest.java
+++ src/test/java/com/bancom/bancom/controller/UsuarioControllerTest.java
@@ -2,22 +2,28 @@ package com.bancom.bancom.controller;
 
 import com.bancom.bancom.entity.Usuario;
 import com.bancom.bancom.repositoy.RepositoryUsuario;
+import com.bancom.bancom.service.ServiceUsuario;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-
+@SpringBootTest
 class UsuarioControllerTest {
 
     @Mock
     RepositoryUsuario repositoryUsuario;
 
+    @Autowired
+    ServiceUsuario serviceUsuario;
+
     @Test
     void guardarUsuario() {
-        /*
+
         Usuario usuario = new Usuario();
         usuario.setId(1L);
         usuario.setName("JUAN");
@@ -25,8 +31,10 @@ class UsuarioControllerTest {
         usuario.setCellphone("333333333");
         usuario.setPassword("3333");
         //repositoryUsuario.save(usuario);
-        when(repositoryUsuario.save(usuario)).thenReturn(usuario);
-        */
+        //when(repositoryUsuario.save(usuario)).thenReturn(usuario);
+        Usuario usuReg = serviceUsuario.guardarUsuario(usuario);
+        assertEquals(1,usuReg.getId() );
+
     }
 
     @Test
